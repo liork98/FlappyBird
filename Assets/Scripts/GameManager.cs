a54f5f3c38008bdc1212af5642d87f92128b9306
Communix <communix@CX14.local>
2023-08-21 20:10:57 +0300

    longer pause

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,24 +13,36 @@ public class GameManager : MonoBehaviour
    private int bird_points;
    public TextMeshProUGUI points_text;
    public GameObject game_over;
+   public GameObject continue_to_menu;
    public string scene_name;
+   public bool waiting_to_click;
    public void Awake()
    {
-      Play();
+      Play(); 
    }
+
+   public void Update()
+   {
+      if (waiting_to_click == true && Input.anyKeyDown)
+      {
+         SceneManager.LoadScene(scene_name);
+      }
+   }
+
    public void AddPoint() //Adding 1 point to the total score when the bird fly through the pipes gap
    {
       bird_points++;
       points_text.text = bird_points.ToString();
    }
-
    public void Play() //Init the game's data and objects
    {
       bird_points = 0;
       points_text.text = bird_points.ToString();
       game_over.SetActive(false);
+      continue_to_menu.SetActive(false);
       Time.timeScale = 1f;
       bird.enabled = true;
+      waiting_to_click = false;
       
       Pipes[] pipes = FindObjectsOfType<Pipes>();
       for (int i = 0; i < pipes.Length; i++)
@@ -41,8 +53,9 @@ public class GameManager : MonoBehaviour
    public void GameOver() //Ending the game after detecting collusion between the bird and obstacle 
    {
       game_over.SetActive(true);
+      continue_to_menu.SetActive(true);
       Pause();
-      SceneManager.LoadScene(scene_name);
+      waiting_to_click = true;
    }
    public void Pause() //Pause the game
    {
